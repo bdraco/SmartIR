116e3f2ae55e37acbc6b134f3b4c2287235199b5
J. Nick Koston <nick@koston.org>
2020-11-13 21:52:27 -1000

    Disable unneeded polling for climate and fan entities
    Since these entities do not implement async_update, there
    is no need to poll

--- custom_components/smartir/climate.py
+++ custom_components/smartir/climate.py
@@ -169,6 +169,11 @@ class SmartIRClimate(ClimateEntity, RestoreEntity):
                                      self._async_power_sensor_changed)
 
     @property
+    def should_poll(self):
+        """Entity does not implement async_update."""
+        return False
+
+    @property
     def unique_id(self):
         """Return a unique ID."""
         return self._unique_id
@@ -288,7 +293,7 @@ class SmartIRClimate(ClimateEntity, RestoreEntity):
         if not self._hvac_mode.lower() == HVAC_MODE_OFF:
             await self.send_command()
 
-        await self.async_update_ha_state()
+        self.async_write_ha_state()
 
     async def async_set_hvac_mode(self, hvac_mode):
         """Set operation mode."""
@@ -298,7 +303,7 @@ class SmartIRClimate(ClimateEntity, RestoreEntity):
             self._last_on_operation = hvac_mode
 
         await self.send_command()
-        await self.async_update_ha_state()
+        self.async_write_ha_state()
 
     async def async_set_fan_mode(self, fan_mode):
         """Set fan mode."""
@@ -306,7 +311,7 @@ class SmartIRClimate(ClimateEntity, RestoreEntity):
         
         if not self._hvac_mode.lower() == HVAC_MODE_OFF:
             await self.send_command()      
-        await self.async_update_ha_state()
+        self.async_write_ha_state()
 
     async def async_turn_off(self):
         """Turn off."""
@@ -375,7 +380,7 @@ class SmartIRClimate(ClimateEntity, RestoreEntity):
             return
 
         self._async_update_temp(new_state)
-        await self.async_update_ha_state()
+        self.async_write_ha_state()
 
     async def _async_humidity_sensor_changed(self, entity_id, old_state, new_state):
         """Handle humidity sensor changes."""
@@ -383,7 +388,7 @@ class SmartIRClimate(ClimateEntity, RestoreEntity):
             return
 
         self._async_update_humidity(new_state)
-        await self.async_update_ha_state()
+        self.async_write_ha_state()
 
     async def _async_power_sensor_changed(self, entity_id, old_state, new_state):
         """Handle power sensor changes."""
@@ -392,13 +397,13 @@ class SmartIRClimate(ClimateEntity, RestoreEntity):
 
         if new_state.state == STATE_ON and self._hvac_mode == HVAC_MODE_OFF:
             self._on_by_remote = True
-            await self.async_update_ha_state()
+            self.async_write_ha_state()
 
         if new_state.state == HVAC_MODE_OFF:
             self._on_by_remote = False
             if self._hvac_mode != HVAC_MODE_OFF:
                 self._hvac_mode = HVAC_MODE_OFF
-            await self.async_update_ha_state()
+            self.async_write_ha_state()
 
     @callback
     def _async_update_temp(self, state):
